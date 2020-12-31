55b37c817c6f19664da124ec7efbab06caef2ff9
David Rothbauer <davidrothbauer2003@gmail.com>
2020-12-31 18:25:22 +0100

    Added storage permission popup

--- app/src/main/java/com/david/ardfmanager/MainMenuActivity.java
+++ app/src/main/java/com/david/ardfmanager/MainMenuActivity.java
@@ -1,11 +1,16 @@
 package com.david.ardfmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.Toast;
 
 import com.david.ardfmanager.event.EventsManagerActivity;
@@ -26,8 +31,12 @@ public class MainMenuActivity extends AppCompatActivity {
         eventsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainMenuActivity.this, EventsManagerActivity.class);
-                startActivity(intent);
+                if(checkPermissions()) {
+                    Intent intent = new Intent(MainMenuActivity.this, EventsManagerActivity.class);
+                    startActivity(intent);
+                }else{
+                    askForStoragePermission();
+                }
             }
         });
 
@@ -46,4 +55,31 @@ public class MainMenuActivity extends AppCompatActivity {
         });
 
     }
+
+    private boolean checkPermissions(){
+        return ContextCompat.checkSelfPermission(MainMenuActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void askForStoragePermission(){
+        ActivityCompat.requestPermissions(MainMenuActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(MainMenuActivity.this, EventsManagerActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainMenuActivity.this, R.string.storage_permission_not_granted, Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+
+
 }
